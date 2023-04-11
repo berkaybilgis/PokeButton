cf1b59ec7c2ba6bd06a2ffe3ea6a4eaf66f309b6
berkaybilgis <berkaybilgis8@gmail.com>
2023-04-11 19:19:08 +0300

    buttonlar eklendi ve stilleri ayarlandı

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
-import { ExampleComponent } from 'pokebutton'
+import { Button } from 'pokebutton'
 import 'pokebutton/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      {/* button tipleri test edildi */}
+      <Button type='primary' text='Primary Button' />
+      <Button type='dashed' text='Dashed Button' />
+      <Button type='text' text='Text Button' />
+      <Button type='link' text='Link Button' />
+      <Button text='Default Button' />
+    </>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const Button = (props) => {
+  // type değerinin durumuna göre button tip değişikliği tanımlandı
+  if (props.type === 'primary') {
+    return <button className={styles.primary}>{props.text}</button>
+  } else if (props.type === 'dashed') {
+    return <button className={styles.dashed}>{props.text}</button>
+  } else if (props.type === 'text') {
+    return <button className={styles.text}>{props.text}</button>
+  } else if (props.type === 'link') {
+    return <button className={styles.link}>{props.text}</button>
+  } else return <button className={styles.default}>{props.text}</button>
 }
